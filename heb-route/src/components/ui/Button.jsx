e93f3b3d35f2f7bc6cf7bfd6a34a11e2700dd384
ongorio <ongorio5@gmail.com>
2023-05-15 10:51:14 -0600

    corrected button

--- heb-route/src/components/ui/Button.jsx
+++ heb-route/src/components/ui/Button.jsx
@@ -16,7 +16,7 @@ export const Button = ({
 }) => {
     return (
         <button style={styles} className={`btn btn-${variant} ${disabled && 'btn-disabled'}`} onClick={() => {if (!disabled && !loading) callbackFunction()}} type={type} disabled={disabled || loading}>
-            {loading ? <Spinner style={{ width: 23, height: 23, transform: 'translateY(-2px)' }}/> : children}
+            {loading ? <Spinner/> : children}
         </button>
     );
 };
