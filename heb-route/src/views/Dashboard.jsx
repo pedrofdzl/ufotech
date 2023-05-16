9fa2f0a4bdc812b62dd06629cc8e57cd091cde6f
ongorio <ongorio5@gmail.com>
2023-05-16 12:32:22 -0600

    Added search bar

--- heb-route/src/views/Dashboard.jsx
+++ heb-route/src/views/Dashboard.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { useNavigate, createSearchParams, Link } from 'react-router-dom';
 
 // Providers
 import { ProductContext } from "../providers/ProductProvider";
@@ -21,6 +21,20 @@ import { BsArrowRightCircle } from 'react-icons/bs';
 const Dashboard = () => {
   const navigate = useNavigate();
   const { categories } = useContext(ProductContext);
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+  const searchHandler = event=>{
+    event.preventDefault();
+    navigate({
+      pathname: '/search',
+      search: `?${createSearchParams({search:searchTerm})}`
+    })
+  };
+
+  const onSearchChange = event=>{
+    setSearchTerm(event.target.value);
+  };
 
   return (
     <>
@@ -28,15 +42,20 @@ const Dashboard = () => {
       <SlLocationPin />
         <Text variant={'b2'}>Sucursal Av. Manuel Gómez Morín</Text>
       </div>
+      <div>
+        <form onSubmit={searchHandler}>
+          <input type="text" name="search" placeholder="Leche, Huevo, Cereal..." onChange={onSearchChange} value={searchTerm}/>
+        </form>
+      </div>
       <Text variant={'h2'}>Categorias</Text>
       
       <div className="category-card-carousel">
         {Object.keys(categories.categories).map(category => {
           return (
-            <a className='category-card' style={{ backgroundColor: categories.categories[category].color }} onClick={() => navigate(`/categories/${category}`)} key={category}>
+            <Link className='category-card' style={{ backgroundColor: categories.categories[category].color }} to={`/categories/${category}`} key={category}>
               <h1>{categories.categories[category].emoji}</h1>
               <h4>{categories.categories[category].name}</h4>
-            </a>
+            </Link>
           );
         })}
       </div>
@@ -49,18 +68,18 @@ const Dashboard = () => {
             <div className='product-card-carousel'>
               {categories.categories[category].products.slice(0, 5).map(product => {
                 return(
-                  <a className="product-card" onClick={() => navigate(`/products/${category}/${product.id}`)} key={product.id}>
+                  <Link className="product-card" to={`/products/${category}/${product.id}`} key={product.id}>
                     <img src={product['Link Imagen']} alt={product.Nombre} width={100} height={100} />
                     <h4>{truncate(product.Nombre, 32)}</h4>
                     <small>${product.Precio}</small>
                     <small className="product-card-unit">{product.Capacidad} {product.Unidad}</small>
-                  </a>
+                  </Link>
                 );
               })}
-              <a className="product-category-see-more" onClick={() => navigate(`/categories/${category}`)}>
+              <Link className="product-category-see-more" to={`/categories/${category}`} >
                 <BsArrowRightCircle className="product-category-see-more-icon" />
                 <Text variant="b4">Ver más</Text>
-              </a>
+              </Link>
           </div>
         </div>
         );
