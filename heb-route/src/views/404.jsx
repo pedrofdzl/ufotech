8b23d57c3965900cf6cbd47c2ef882ee93aceefc
pedrofdzl <pedroandresfdz@hotmail.com>
2023-05-30 11:43:12 -0600

    styled view correctly

--- heb-route/src/views/404.jsx
+++ heb-route/src/views/404.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Components
+import { Text } from "../components/ui/Text";
 
-const Error404 = props => {
-    return <>
-        <h1>404</h1>
-        <h4>We couldn't find the page you were looking for! Go to <Link to={'/'} replace >Dashboard</Link> </h4>
-    </>
+// Stylesheets
+import '../stylesheets/Errors.css';
+
+const Error404 = () => {
+    return (
+        <div className='error-container'>
+            <div className='centered-container'>
+                <Text>Error 404</Text>
+                <Text variant={'b2'}>Parece que la pagina que estas buscando no existe, regresa al <Link to={'/'} replace ><span>Dashboard</span></Link> </Text>
+            </div>
+        </div>
+    );
 }
 
 export default Error404;
