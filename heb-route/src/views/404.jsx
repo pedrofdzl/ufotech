655d374c614f0bc4bb9207360b51f982d48da1a0
ongorio <ongorio5@gmail.com>
2023-06-02 01:52:01 -0600

    corrected error pages

--- heb-route/src/views/404.jsx
+++ heb-route/src/views/404.jsx
@@ -11,8 +11,8 @@ const Error404 = () => {
     return (
         <div className='error-container'>
             <div className='centered-container'>
-                <Text>Error 404</Text>
-                <Text variant={'b2'}>Parece que la pagina que estas buscando no existe, regresa al <Link to={'/'} replace ><span>Dashboard</span></Link> </Text>
+                <Text>Pagina No Encontrada!</Text>
+                <Text variant={'b2'}>Parece que la página que estas buscando no existe, regresa al <Link to={'/'} replace ><span>Dashboard</span></Link> </Text>
             </div>
         </div>
     );
